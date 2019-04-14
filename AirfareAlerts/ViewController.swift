a7044c643e28bcd9114fdffc263dfb9d64af2568
Elina Talashka <elt@infare.com>
2019-04-14 12:35:12 +0200

    Initial Commit

---
+++ AirfareAlerts/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  AirfareAlerts
+//
+//  Created by Elina Talashka on 14/04/2019.
+//  Copyright © 2019 Elina Talashka. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
